646dbc30d831aea36ceebdc9236dd99d766fe781
김영준 <102145868+kimyj0530@users.noreply.github.com>
2023-02-03 23:05:01 +0900

    Create 11.c

---
+++ 11.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int n, i, cnt=0, tmp;
+	scanf("%d", &n);
+	for(i=1; i<=n; i++){
+		tmp=i;
+		while(tmp>0){
+			tmp=tmp/10;
+			cnt++;
+		}
+	}
+	printf("%d\n", cnt);
+	return 0;
+}
